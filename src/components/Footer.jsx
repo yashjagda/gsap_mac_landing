91a47998e4c8587b042bd93e2b7df5d619f082cd
yashjagda <37515447+yashjagda@users.noreply.github.com>
2025-12-08 19:58:33 -0500

    Footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import {footerLinks} from "../constants/index.js";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <footer>
+            <div className="info">
+                <p>More ways to shop: Find an Apple Store or other retailer near you. Or call 000800 040 1966.</p>
+                <img src="/logo.svg" alt ="Apple logo"/>
+            </div>
 
+            <hr />
+
+            <div className="links">
+                <p>YJ clone attempt V1</p>
+
+                <ul>
+                    {footerLinks.map(({label, link }) => (
+                        <li key={label}>
+                            <a href={link}>{label}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </footer>
+    )
+}
 export default Footer
